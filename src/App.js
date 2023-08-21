39712dec5cc72686c7ba787f385003a3801e0879
maximuix <daretag1617@gmail.com>
2023-08-21 17:55:04 +0300

    up script

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {useEffect} from "react";
 
+const tg = window.Telegram.WebApp
 
 function App() {
-
+    useEffect(() => {
+        tg.ready()
+    })
     return (
 
         <div className="App">
