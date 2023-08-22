bdd7e038a6ae638de083439525c4ad5ba8351426
maximuix <daretag1617@gmail.com>
2023-08-22 23:38:35 +0300

    up script

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ function App() {
 
     useEffect(() => {
         tg.ready()
-    })
+    }, [])
     return (
 
         <div className="App">
             <Header/>
             <Routes>
-                <Route index element={<ProductList/>}/>
-                <Route path="form" element={<Form/>}/>
+                <Route index element={<ProductList />}/>
+                <Route path="form" element={<Form />}/>
             </Routes>
             <button onClick={onToggleButton}>Toggle</button>
         </div>
