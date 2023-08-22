e9c1d04795a73f722e65e6b4c7d86fab2d0ba847
maximuix <daretag1617@gmail.com>
2023-08-23 00:13:30 +0300

    up script

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -21,7 +21,7 @@ const Form = () => {
         } else {
             tg.MainButton.show()
         }
-    }, [])
+    }, [country, street])
 
     const onChangeCountry = (e) => {
         SetCountry(e.target.value)
